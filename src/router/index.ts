88361a0f1c78d210f61495bff103468a0fecd31a
saugatmalla <saugat013@gmail.com>
2020-10-08 19:15:46 -0400

    routes setup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
+import Login from '../views/NotLogged/Login.vue'
+import Register from '../views/NotLogged/Register.vue'
+import Dashboard from '../views/Logged/Dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
     path: '/',
     name: 'Dashboard',
     component: Dashboard
-  },
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/NotLogged/Login.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <div class="center-container">
+        <div class="center-content">
+            Login
+        </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+@Component
+export default class Login extends Vue {
+
+}
+</script>
+<style>
+.center-container {
+    height: 100vh;
+    position: relative;
+}
+.center-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+</style>
