c5c2e81f4a2c234b035c76f6803d61640c166e45
saugatmalla <saugat013@gmail.com>
2020-10-08 22:06:41 -0400

    login page setup

--- src/views/NotLogged/Login.vue
+++ src/views/NotLogged/Login.vue
@@ -1,29 +1,45 @@
 <template>
-<div>
-    <div class="center-container">
-        <div class="center-content">
-            Login
+  <div>
+    <section class="hero is-fullheight has-text-centered">
+      <div class="hero-body">
+        <div class="container">
+          <p class="is-size-1" style="margin-bottom: 40px">Login</p>
+          <section>
+            <b-field label="Email">
+              <b-input type="email" placeholder="Email" v-model="user.email"> </b-input>
+            </b-field>
+
+            <b-field label="Password">
+              <b-input type="password" placeholder="Password" v-model="user.password" password-reveal> </b-input>
+            </b-field>
+
+            <b-button type="is-primary is-light" rounded class="mr4" @click="login">
+              Login
+            </b-button>
+            <div>
+              Don't have an account? <span><u>Sign up</u></span>
+            </div>
+          </section>
         </div>
-    </div>
-</div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class Login extends Vue {
 
+  user: object = {
+    email: null,
+    password: null
+  }
+
+  login(): void {
+    this
+  }
 }
 </script>
 <style>
-.center-container {
-    height: 100vh;
-    position: relative;
-}
-.center-content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-}
 </style>
